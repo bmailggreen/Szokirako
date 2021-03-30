c9a8711e4a1b4ca9a8e702b2020cccdbf6b39519
Gerjak Balazs <bmailggreen@gmail.com>
2021-03-30 19:06:03 +0200

    Added a few input checker test for GameData.

--- src/com/company/GameDataTest.java
+++ src/com/company/GameDataTest.java
@@ -2,9 +2,46 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class GameDataTest {
 
     @Test
-    void getGuessNumber() {
+    void correctInitialGuessNumber() {
+        GameData gd = new GameData(10, "Harry Potter");
+        assertEquals(10, gd.getGuessNumber());
     }
+
+    @Test
+    void correctActualTextAfterInitialisation() {
+        GameData gd = new GameData(10, "Harry Potter");
+        assertEquals("_____ ______", new String(gd.getActualText()) );
+    }
+
+    @Test
+    void correctRemainingCharactersAfterInitialisation() {
+        GameData gd = new GameData(10, "Harry Potter");
+        assertEquals("abcdefghijklmnopqrstuvwxyzáéíóöőúüű", new String(gd.getRemainingCharacters()));
+    }
+
+    @Test
+    void correctRemainingCharactersAfterTriedA() {
+        GameData gd = new GameData(10, "Harry Potter");
+        gd.setTriedCharacter('a');
+        assertEquals("bcdefghijklmnopqrstuvwxyzáéíóöőúüű", new String(gd.getRemainingCharacters()));
+    }
+
+    @Test
+    void correctGuessedCharactersAfterInitialisation() {
+        GameData gd = new GameData(10, "Harry Potter");
+        assertEquals("", new String(gd.getGuessedCharacters()));
+    }
+
+    @Test
+    void correctGuessedCharactersAfterGuessingP() {
+        GameData gd = new GameData(10, "Harry Potter");
+        gd.setTriedCharacter('p');
+        assertEquals("p", new String(gd.getGuessedCharacters()));
+    }
+
 }
