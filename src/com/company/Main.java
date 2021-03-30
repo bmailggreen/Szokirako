3221bf9a7287c3d89d775fd83ae5c561b49d4899
Andras <csolandris@gmail.com>
2021-03-30 18:07:45 +0200

    Increase the number of guesses.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        GameData gd = new GameData(5, "Harry Potter");
+        GameData gd = new GameData(6, "Harry Potter");
         CharacterDisplay cdp = new CharacterDisplay();
         Game game = new Game(gd, cdp);
         game.startGame();
